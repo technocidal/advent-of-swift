c01c48788d9ecadea9a4aa775afc57b4ebbe6df1
Johannes Ebeling <14994778+technocidal@users.noreply.github.com>
2022-12-02 22:29:20 +0100

    ✨ Implement solution for the second day

--- Sources/AdventOfSwift/AdventOfSwift.swift
+++ Sources/AdventOfSwift/AdventOfSwift.swift
@@ -9,7 +9,8 @@ struct AdventOfSwift: AsyncParsableCommand {
         abstract: "Solutions for AoC problems",
         subcommands: [
             Leaderboard.self,
-            First.self
+            First.self,
+            Second.self
         ]
     )
 }
@@ -88,8 +89,82 @@ struct First: AsyncParsableCommand {
             .sorted()
             .suffix(3)
             .reduce(into: 0) { $0 += $1 }
-
+        
         print("The top three elves are carrying \(lastThree) calories.")
     }
 }
 
+struct Second: AsyncParsableCommand {
+    
+    func run() async throws {
+        let input = try await getInput(.init(day: 2, year: 2022))
+        
+        var rounds = input.components(separatedBy: .newlines)
+        _ = rounds.removeLast()
+        var score1 = rounds.reduce(into: 0) { $0 += getRoundScore($1) }
+        
+        print("Total score for the first part \(score1)")
+        
+        var score2 = rounds.map({ getActualRound($0) }).reduce(into: 0) { $0 += getRoundScore($1) }
+        
+        print("Total score for the second part \(score2)")
+    }
+    
+    private func checkWinning(_ input: String) -> Bool {
+        ["A Y", "B Z", "C X"].contains([input])
+    }
+    
+    private func checkTie(_ input: String) -> Bool {
+        ["A X", "B Y", "C Z"].contains([input])
+    }
+    
+    private func getBaseScore(_ input: String) -> Int {
+        switch input {
+        case "X":
+            return 1
+        case "Y":
+            return 2
+        default:
+            return 3
+        }
+    }
+    
+    private func getRoundScore(_ input: String) -> Int {
+        var score = 0
+        if checkWinning(input) {
+            score += 6
+        } else if checkTie(input) {
+            score += 3
+        }
+        
+        score += getBaseScore(input.components(separatedBy: .whitespaces)[1])
+        return score
+    }
+    
+    private func getActualRound(_ input: String) -> String {
+        let components = input.components(separatedBy: .whitespaces)
+        let opponent = components[0]
+        let round = components[1]
+        switch round {
+        case "X":
+            switch opponent {
+            case "A": return "A Z"
+            case "B": return "B X"
+            default: return "C Y"
+            }
+        case "Z":
+            switch opponent {
+            case "A": return "A Y"
+            case "B": return "B Z"
+            default: return "C X"
+            }
+            
+        default:
+            switch opponent {
+            case "A": return "A X"
+            case "B": return "B Y"
+            default: return "C Z"
+            }
+        }
+    }
+}
